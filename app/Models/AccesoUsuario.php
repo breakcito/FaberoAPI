8b779845129d471db91b2abe4fcf58e18341961c
PapuLap <bruce.sanchez.1164@gmail.com>
2026-08-03 01:06:51 -0500

    commit fake

--- app/Models/AccesoUsuario.php
+++ app/Models/AccesoUsuario.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 // tabla que permite enlazar un usuario con un acceso por cada vista
 // logrando hacer algo como que un usuario tenga el acceso de realizar o no
 // una accion por vista
+
 class AccesoUsuario extends Model
 {
     protected $table = 'acceso_usuario';
